8685fac7110dd4b89ce993b5a2950ff93fc13480
André Vega <ventaslederer@hotmail.com>
2022-03-28 20:34:37 -0600

    Added create and update user controller.

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,29 +1,58 @@
-const {User} = require("../models")
+const { User } = require("../models");
 
 const userController = {
   getAllUsers: async (req, res) => {
     try {
       const userDB = await User.find()
-      res.json(userDB)
+      .populate("thoughts")
+      .populate("friends");
+      res.json(userDB);
     } catch (err) {
-      console.log(err)
-      res.status(500).json(err)
+      console.log(err);
+      res.status(500).json(err);
     }
   },
   getSingleUser: async (req, res) => {
     try {
       const userDB = await User.findById(req.params.id)
-      .populate("thoughts")
-      .populate("friends")
-      res.json(userDB)
+        .populate("thoughts")
+        .populate("friends");
+      res.json(userDB);
     } catch (err) {
-      console.log(err)
-      res.status(500).json(err)
-    } 
-  }
-}
-
-
-
+      console.log(err);
+      res.status(500).json(err);
+    }
+  },
+  createUser: async (req, res) => {
+    try {
+      const newUser = await User.create(req.body);
+      // Example data
+      // {
+      //   "username": "lernantino",
+      //   "email": "lernantino@gmail.com"
+      // }
+      res.json(newUser);
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  },
+  updateUser: async (req, res) => {
+    try {
+      const userDB = await User.findOneAndUpdate(
+        { _id: req.params.id },
+        { $set: req.body },
+        { runValidators: true, new: true }
+      );
+      //Will this work?
+      !userDB
+        ? res.status(404).json({ message: 'No user with this id!' })
+        : res.json(userDB)
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  },
+};
 
 module.exports = userController;
